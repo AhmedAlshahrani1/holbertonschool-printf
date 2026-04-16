1d4378e6b7b7507e466d03e51670747ecdb3265f
dana <trivian1966@gmail.com>
2026-04-16 15:41:06 +0000

    Function for the 1st task

--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 
 int print_str(char *str);
+int print_number(int n);
 
 /**
  * print_str - prints a string
@@ -24,6 +25,30 @@ int print_str(char *str)
 }
 
 /**
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * Return: number of characters printed
+ */
+int print_number(int n)
+{
+	unsigned int num;
+	int chars = 0;
+
+	if (n < 0)
+	{
+		chars += _putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num / 10)
+		chars += print_number(num / 10);
+	chars += _putchar((num % 10) + '0');
+	return (chars);
+}
+
+/**
  * _printf - produces output according to a format
  * @format: character string containing zero or more directives
  *
@@ -41,28 +66,29 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 			_putchar(format[i]);
-		else if (format[i + 1] == '\0')
-		{
-			va_end(args);
-			return (-1);
-		}
-		else if (format[i + 1] == 'c')
-		{
-			_putchar(va_arg(args, int));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			chars += print_str(va_arg(args, char *)) - 1;
-			i++;
-		}
-		else if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-		}
 		else
-			_putchar(format[i]);
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			if (format[i] == 'c')
+				_putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				chars += print_str(va_arg(args, char *)) - 1;
+			else if (format[i] == 'd' || format[i] == 'i')
+				chars += print_number(va_arg(args, int)) - 1;
+			else if (format[i] == '%')
+				_putchar('%');
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				chars++;
+			}
+		}
 		chars++;
 		i++;
 	}
