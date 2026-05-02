9e10a858c382f7ce0697f78d9576258f862ab5eb
dana <trivian1966@gmail.com>
2026-05-02 16:03:11 +0000

    printf edits

--- _printf.c
+++ _printf.c
@@ -1,9 +1,78 @@
 #include "main.h"
 
 /**
+ * print_number - prints a signed integer with width/flags
+ */
+
+int print_number(va_list args, flags_t *f, int w)
+{
+	long int n = va_arg(args, int);
+	long int tmp = n;
+	int len = 0, i, pad;
+	char buf[20];
+
+	if (n < 0) {
+		len++;
+		tmp = -tmp;
+	}
+	if (tmp == 0) buf[len++] = '0';
+	while (tmp > 0) {
+		buf[len++] = (tmp % 10) + '0';
+		tmp /= 10;
+	}
+
+	pad = (w > len) ? (w - len) : 0;
+	if (f->minus == 0)
+		for (i = 0; i < pad; i++) _putchar(' ');
+
+	if (n < 0) _putchar('-');
+	for (i = len - 1; i >= (n < 0 ? 1 : 0); i--) _putchar(buf[i]);
+
+	if (f->minus == 1)
+		for (i = 0; i < pad; i++) _putchar(' ');
+
+	return (len > w ? len : w);
+}
+/**
+ * print_base - helper for unsigned, octal, and hex
+ */
+int print_base(unsigned int n, int base, int uppercase, flags_t *f, int w)
+{
+	char *set = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+	char buf[64];
+	int len = 0, i, pad;
+
+	if (n == 0) buf[len++] = '0';
+	while (n > 0) {
+		buf[len++] = set[n % base];
+		n /= base;
+	}
+
+	pad = (w > len) ? (w - len) : 0;
+	if (f->minus == 0)
+		for (i = 0; i < pad; i++) _putchar(' ');
+	for (i = len - 1; i >= 0; i--) _putchar(buf[i]);
+	if (f->minus == 1)
+		for (i = 0; i < pad; i++) _putchar(' ');
+
+	return (len > w ? len : w);
+}
+
+/* Specific wrappers for the references needed */
+int print_unsigned(va_list args, flags_t *f, int w)
+{ return print_base(va_arg(args, unsigned int), 10, 0, f, w); }
+
+int print_octal(va_list args, flags_t *f, int w)
+{ return print_base(va_arg(args, unsigned int), 8, 0, f, w); }
+
+int print_hex(va_list args, flags_t *f, int w)
+{ return print_base(va_arg(args, unsigned int), 16, 0, f, w); }
+
+int print_HEX(va_list args, flags_t *f, int w)
+{ return print_base(va_arg(args, unsigned int), 16, 1, f, w); }
+
+/**
  * _printf - produces output according to a format
- * @format: character string containing zero or more directives
- * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
